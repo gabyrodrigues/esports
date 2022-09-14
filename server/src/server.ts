cca71168a9fe655f0e4a4349ebcb556d3515bb50
Gabriely <gabriely.lima@nave.rs>
2022-09-13 23:16:13 -0300

    setting basic server routes and creating database models

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,27 @@
-import express, { response } from 'express'
+import express, { response } from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: 'Ad 1'},
-        {id: 2, name: 'Ad 2'},
-        {id: 3, name: 'Ad 3'},
-        {id: 4, name: 'Ad 4'},
-    ])
-})
 
-app.listen(3333)
+app.get('/games', (request, response) => {
+	return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+	return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Ad 1' },
+    { id: 2, name: 'Ad 2' },
+    { id: 3, name: 'Ad 3' },
+    { id: 4, name: 'Ad 4' },
+  ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([]);
+});
+
+app.listen(3333);
