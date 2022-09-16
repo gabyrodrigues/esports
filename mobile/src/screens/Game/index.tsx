9085a7bc59ce8243f25a8803bb7507b54c459de6
Gabriely <gabriely.lima@nave.rs>
2022-09-15 22:14:06 -0300

    create mobile discord modal fetching from backend and copying discord to clipboard

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -15,9 +15,11 @@ import { THEME } from '../../theme'
 import logoImg from '../../assets/logo-nlw-esports.png'
 import Heading from '../../components/Heading'
 import { DuoCard, DuoCardProps } from '../../components/DuoCard'
+import { DuoMatch } from '../../components/DuoMatch'
 
 export function Game() {
 	const [duos, setDuos] = useState<DuoCardProps[]>([])
+	const [discordDuoSelected, setDiscordDuoSelected] = useState('')
 
 	const navigation = useNavigation()
 
@@ -26,6 +28,14 @@ export function Game() {
 
 	function handleGoBack() {
 		navigation.goBack()
+	}
+
+	async function getDiscordUser(adsId: string) {
+		fetch(`http://192.168.100.24:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => {
+      setDiscordDuoSelected(data.discord)
+    })
 	}
 
 	useEffect(() => {
@@ -74,7 +84,7 @@ export function Game() {
 						renderItem={({ item }) => (
 							<DuoCard 
 								data={item}
-								onConnect={() => {}}
+								onConnect={() => getDiscordUser(item.id)}
 							/>
 						)}
 						showsHorizontalScrollIndicator={false}
@@ -86,6 +96,12 @@ export function Game() {
 								Não há anúncios publicados ainda.
 							</Text>
 						)}
+					/>
+
+					<DuoMatch
+						visible={discordDuoSelected.length > 0}
+						discord={discordDuoSelected}
+						onClose={() => setDiscordDuoSelected('')}
 					/>
 				</>
 			</SafeAreaView>
